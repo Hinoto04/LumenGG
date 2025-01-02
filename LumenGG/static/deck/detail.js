9af57ef2b5df9b5611f4370fd3c35ecd09bd3c94
Hinoto04 <hinoto04@kakao.com>
2025-01-02 23:53:08 +0900

    사이드덱 확인 기능 추가

--- LumenGG/static/deck/detail.js
+++ LumenGG/static/deck/detail.js
@@ -14,7 +14,23 @@ function splitElements() {
     
     // 오리지널 제거
     $original.remove()
-  }
-  
+}
+
   // 함수 실행
-  splitElements();
+splitElements();
+
+$(document).ready(function () {
+$('.hoverOn').each(function () {
+    const originalHeight = $(this).height(); // 각 부모의 초기 높이 저장
+
+    $(this).hover(
+    function () {
+        const childHeight = $(this).find('.sideImg').outerHeight(); // 자식 요소 높이 계산
+        $(this).css('height', childHeight); // 부모 높이를 증가
+    },
+    function () {
+        $(this).css('height', originalHeight); // 부모 높이를 초기 높이로 복원
+    }
+    );
+});
+});
