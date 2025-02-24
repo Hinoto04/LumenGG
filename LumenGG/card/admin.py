ff13705087d514a6981efb63414f3a8ee42dc198
Hinoto04 <hinoto04@kakao.com>
2025-02-25 03:10:40 +0900

    어드민 페이지 카드 검색 오류

--- LumenGG/card/admin.py
+++ LumenGG/card/admin.py
@@ -4,7 +4,7 @@ from .models import Character, Card
 class CardAdmin(admin.ModelAdmin):
     list_display = ('name', 'character', 'type')
     list_filter = ('character', 'type')
-    search_fields = ('name', 'character', 'pos', 'body', 'special', 'frame', 'code')
+    search_fields = ('name', 'character__name', 'pos', 'body', 'special', 'frame', 'code')
 
 # Register your models here.
 admin.site.register(Character)
