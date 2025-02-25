ec4e1d1faca1db8f27012cdfe380ff35a8f9fb65
Hinoto04 <hinoto04@kakao.com>
2025-02-25 17:48:03 +0900

    QNA 및 덱 목록 디자인 개편

--- LumenGG/qna/forms.py
+++ LumenGG/qna/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class QnaSearchForm(forms.Form):
     query = forms.CharField(max_length=255, required=True, label='Search', 
-                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Search'}))
+                            widget=forms.TextInput(attrs={'class': 'form-control 배경색1', 'placeholder': '검색'}))
