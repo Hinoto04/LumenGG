0cc2c17cd6a4285e13c10a10d64872fd61a4ac54
Hinoto04 <hinoto04@kakao.com>
2025-04-05 02:20:01 +0900

    카드상세에서 다른판본 볼 수 있도록

--- LumenGG/static/card/detail.js
+++ LumenGG/static/card/detail.js
@@ -5,3 +5,12 @@ $("#kwSelect").on("change", function() {
 
 //console.log($("#kwSelect").val())
 $("#"+$("#kwSelect").val()).css('display', 'grid');
+
+var imgList = [];
+var now = 0;
+
+$("#카드이미지").on("click", function() {
+    now++;
+    if(now >= imgList.length) now = 0;
+    $("#카드이미지").attr("src", imgList[now]);
+});
