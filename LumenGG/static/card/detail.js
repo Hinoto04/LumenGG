907016f8ec186c712928e90bdbd84a0791d38ad6
Hinoto04 <hinoto04@kakao.com>
2025-04-07 19:42:42 +0900

    카드 상세 페이지 이미지 변경 버튼

--- LumenGG/static/card/detail.js
+++ LumenGG/static/card/detail.js
@@ -6,11 +6,6 @@ $("#kwSelect").on("change", function() {
 //console.log($("#kwSelect").val())
 $("#"+$("#kwSelect").val()).css('display', 'grid');
 
-var imgList = [];
-var now = 0;
-
 $("#카드이미지").on("click", function() {
-    now++;
-    if(now >= imgList.length) now = 0;
-    $("#카드이미지").attr("src", imgList[now]);
+    imageChange();
 });
