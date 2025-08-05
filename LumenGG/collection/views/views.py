00f597b6ba1fb2fcb6018ce478cb973dc770e1e0
Hinoto <hinoto04@kakao.com>
2025-08-05 18:31:49 +0900

    이오몽 중립 카드 검색

--- LumenGG/collection/views/views.py
+++ LumenGG/collection/views/views.py
@@ -27,6 +27,7 @@ def index(req):
         q1.add(Q(card__character=form.data.get('char')), q.OR)
         try:
             q1.add(Q(name__contains=Character.objects.get(id=form.data.get('char')).name)&Q(card=None), q.OR)
+            q1.add(Q(name__contains="("+Character.objects.get(id=form.data.get('char')).name), q.OR)
         except:
             pass
         q.add(q1, q.AND)
